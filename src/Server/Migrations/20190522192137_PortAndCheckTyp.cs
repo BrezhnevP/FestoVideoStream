7799654488fae4ae017e4a6cbcc83b85cd1f2740
T1kon <surinnik@gmail.com>
2019-05-22 22:45:07 +0300

    Added new ef migration

---
+++ src/Server/Migrations/20190522192137_PortAndCheckTyp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FestoVideoStream.Migrations
+{
+    public partial class PortAndCheckTyp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CheckType",
+                table: "Devices",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastStreamStatusUpdate",
+                table: "Devices",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Port",
+                table: "Devices",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckType",
+                table: "Devices");
+
+            migrationBuilder.DropColumn(
+                name: "LastStreamStatusUpdate",
+                table: "Devices");
+
+            migrationBuilder.DropColumn(
+                name: "Port",
+                table: "Devices");
+        }
+    }
+}
