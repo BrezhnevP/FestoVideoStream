86790e1817cf2633c3dfb48cd8b65189c2ecae67
Сурин Никита Андреевич <surinnik@gmail.com>
2019-05-23 18:33:14 +0300

    Added HLS video player

--- src/Client/src/app/components/device-video/device-video.component.ts
+++ src/Client/src/app/components/device-video/device-video.component.ts
@@ -3,6 +3,8 @@ import { VgAPI } from 'videogular2/core';
 import { VgDASH } from 'videogular2/src/streaming/vg-dash/vg-dash';
 import { VgHLS } from 'videogular2/src/streaming/vg-hls/vg-hls';
 import { HttpClient } from '@angular/common/http';
+import * as HLS from 'hls.js';
+
 
 export interface IMediaStream {
   type: 'dash';
@@ -36,5 +38,21 @@ export class DeviceVideoComponent implements OnInit {
     this.http.get(this.streamUrl + this.deviceId + '/dash', { responseType: 'text' }).subscribe(result => {
       this.currentStream = result;
     }, error => console.error(error));
+
+    var video = <HTMLVideoElement>document.getElementById('video');
+    if(hls.isSupported()) {
+      var hls = new HLS();
+      hls.loadSource(this.currentStream);
+      hls.attachMedia(video);
+      hls.on(hls.Events.MANIFEST_PARSED,function() {
+        video.play();
+    });
+  }
+    else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+      video.src = 'currentStream';
+      video.addEventListener('loadedmetadata',function() {
+        video.play();
+      });
+    }
   }
 }
